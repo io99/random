3d53fa3c055b2c3dad9830ed113d70b15680e33c
io99 <31932394+io99@users.noreply.github.com>
2017-09-29 19:59:36 +0530

    Add files via upload

---
+++ jscalculator.js
@@ -0,0 +1,34 @@
+var Calculator{
+number1: X,
+number2: X,
+add:function(){
+ this.result = this.number1+this.number2;
+	}
+Calculator.add();
+Calculator.result
+}
+
+
+
+var Calculator {
+	operand1: X,
+	operand2,
+	add:function(){
+	this.result =this.operand1+this.operand2;
+
+	}
+}
+Calculator.add();
+Calculator.result
+
+
+var Calculator{
+	operand1: X,
+	operand2: Y,
+	add:function(){
+this.result= this.operand1+this.operand1;
+
+	}
+}
+Calculator.add();
+Calculator.result
